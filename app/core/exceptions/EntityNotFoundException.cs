5b04b53e07be48f367b7c18667a502e1a02ebfb2
Rian Finnegan <rian@amphoradata.com>
2021-07-15 11:21:26 +1000

    print the ID when not found

--- app/core/exceptions/EntityNotFoundException.cs
+++ app/core/exceptions/EntityNotFoundException.cs
@@ -7,18 +7,22 @@ namespace SignalBox.Core
         public EntityNotFoundException(Type t, string commonId, Exception ex) : base($"Entity of type {t.Name} with id {commonId} not found", ex)
         {
             this._entityType = t;
+            this._entityId = commonId;
         }
         public EntityNotFoundException(Type t, long id, Exception ex) : base("Not Found", $"Entity of type {t.Name} with Id {id} not found", ex)
         {
             this._entityType = t;
+            this._entityId = id.ToString();
         }
         public EntityNotFoundException(Type t, long id, string message) : base($"Entity of type {t.Name} with Id {id} not found", message)
         {
             this._entityType = t;
+            this._entityId = id.ToString();
         }
 
         private Type _entityType;
+        private string _entityId;
         public override int Status => 404;
-        public override string Title => $"{_entityType?.Name} Resource Not Found";
+        public override string Title => $"{_entityType?.Name} ({_entityId}) Resource Not Found";
     }
 }
