434b43d6a0323b299341155e7dc019401c3464f2
Rian Finnegan <rian@amphoradata.com>
2021-07-21 15:36:25 +1000

    updated status box again

--- app/web/ClientApp/src/components/molecules/RecommenderStatusBox.js
+++ app/web/ClientApp/src/components/molecules/RecommenderStatusBox.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { NoteBox } from "./NoteBox";
 
-import "./css/recommender-status-box.css"
+import "./css/recommender-status-box.css";
 
+const label = "Recommender Status";
 export const RecommenderStatusBox = ({ recommender }) => {
   if (!recommender.modelRegistration) {
     return (
-      <NoteBox cardBodyClassName="training" label="Recommender Status">
-        Searching for optimum recommendations.
+      <NoteBox cardBodyClassName="training" label={label}>
+        Exploring for good recommendations.
       </NoteBox>
     );
   } else {
-
-      return <NoteBox cardBodyClassName="ready" label="Status">Exploiting prior knowledge.</NoteBox>;
+    return (
+      <NoteBox cardBodyClassName="ready" label={label}>
+        Discovered good recommendations.
+      </NoteBox>
+    );
   }
 };
