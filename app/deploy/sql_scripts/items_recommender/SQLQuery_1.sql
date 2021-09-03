03803c421c435b3817171aed022863daf3372134
Rian Finnegan <rian@amphoradata.com>
2021-09-03 11:46:59 +1000

    udpdate temp migrations

--- app/deploy/sql_scripts/items_recommender/SQLQuery_1.sql
+++ app/deploy/sql_scripts/items_recommender/SQLQuery_1.sql
@@ -1,6 +1,8 @@
 -- this should run before the migration
-DELETE from dbo.InvokationLogEntry
-DELETE from dbo.ProductRecommendations
 
--- this may or may not be needed
+DELETE from dbo.InvokationLogEntry
+
+-- these may or may not be needed, depending on the environment.
+-- DELETE from dbo.ProductRecommendations
+-- DELETE from dbo.ProductRecommenders
 -- DELETE from dbo.Products
