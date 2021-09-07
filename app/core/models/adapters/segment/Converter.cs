9dd82d7b97a2fcfb1332124e376c3287bf121de0
Rian Finnegan <rian@amphoradata.com>
2021-09-07 19:13:03 +1000

    add an unknown event in case null

--- app/core/models/adapters/segment/Converter.cs
+++ app/core/models/adapters/segment/Converter.cs
@@ -13,7 +13,7 @@ namespace SignalBox.Core.Adapters.Segment
         public static TrackedUserEventInput ToTrackedUserEventInput(this SegmentModel model, IntegratedSystem sys)
         {
             return new TrackedUserEventInput(
-                model.UserId, model.MessageId, model.Timestamp, null, sys.Id, $"Segment|{model.Type}", model.Event, model.Properties);
+                model.UserId, model.MessageId, model.Timestamp, null, sys.Id, $"Segment|{model.Type}", model.Event ?? "Unknown", model.Properties);
         }
     }
 }
