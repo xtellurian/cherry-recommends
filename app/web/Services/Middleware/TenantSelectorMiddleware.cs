3f4fcdd5d77a2283a08820d0002adf604474c513
Rian Finnegan <rian@amphoradata.com>
2021-10-02 11:50:47 +1000

    ensure that the tenant is set when needed

--- app/web/Services/Middleware/TenantSelectorMiddleware.cs
+++ app/web/Services/Middleware/TenantSelectorMiddleware.cs
@@ -25,12 +25,14 @@ namespace SignalBox.Web.Services
             var tenantProvider = context.RequestServices.GetRequiredService<ITenantProvider>();
             var resolver = context.RequestServices.GetRequiredService<ITenantResolutionStrategy>();
 
+            var req = new HttpRequestModel(context.Request.Path.Value.ToString(),
+                context.Request.Headers.ToDictionary(_ => _.Key, _ => _.Value.ToString()));
+            // this should always be set
+            await tenantProvider.SetTenantName(await resolver.ResolveName(req));
+
+            // only try to authorize the request if the server is multitenant and the request isnt anonymous
             if (!isAllowAnonymous && resolver.IsMultitenant)
             {
-                var req = new HttpRequestModel(context.Request.Path.Value.ToString(),
-                    context.Request.Headers.ToDictionary(_ => _.Key, _ => _.Value.ToString()));
-                await tenantProvider.SetTenantName(await resolver.ResolveName(req));
-
                 var tenant = tenantProvider.Current();
                 var authorizor = context.RequestServices.GetRequiredService<ITenantAuthorizationStrategy>();
                 await authorizor.Authorize(context.User, tenant);
