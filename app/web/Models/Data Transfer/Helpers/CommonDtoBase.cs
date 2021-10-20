cd2a573798b4668bdcd91aaa8a01b61f63d88dcf
Rian <rian@four2.ai>
2021-10-20 22:42:49 +0000

    Merged PR 230: Hotfix: Common Id should allow uppercase
    - update docusarus
    - allow uppercase comon id

--- app/web/Models/Data Transfer/Helpers/CommonDtoBase.cs
+++ app/web/Models/Data Transfer/Helpers/CommonDtoBase.cs
@@ -6,7 +6,7 @@ namespace SignalBox.Web.Dto
     {
         [Required]
         [StringLength(99, MinimumLength = 3)]
-        [RegularExpression(@"^[a-z0-9_\-]+$", ErrorMessage = "Common Id can only contain lowercase letters, numbers, underscore, and hyphen.")]
+        [RegularExpression(@"^[a-zA-Z0-9_\-]+$", ErrorMessage = "Common Id can only contain alphanumeric chars, underscores, and hyphens.")]
         public string CommonId { get; set; }
         public virtual string Name { get; set; }
     }
