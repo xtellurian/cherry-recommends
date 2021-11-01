887480cd08dd52e778bb4785c8cb7c67bd238856
Rian <rian@four2.ai>
2021-11-01 01:06:22 +0000

    Merged PR 239: Webflow DNS
    update DNS for webflow

--- app/deploy/azure-scripts/dns-cherry.ai/core-dns.sh
+++ app/deploy/azure-scripts/dns-cherry.ai/core-dns.sh
@@ -6,14 +6,25 @@ ZONE="cherry.ai"
 RG="dns"
 echo DNS zone is $ZONE
 
+## -------- O365 --------------
+echo "Setting Outlook DNS"
 # set CName for Outlook autodiscover
 az network dns record-set cname set-record -g $RG -z $ZONE -n autodiscover -c "autodiscover.outlook.com"
 
 # Create an MX record for outlook email
 az network dns record-set mx add-record -g $RG -z $ZONE -n @ -e "cherry-ai.mail.protection.outlook.com" -p 0
 
-# add O365 
+# add O365 security
 az network dns record-set txt add-record -g $RG -z $ZONE -n @ -v "MS=ms74302510"
 az network dns record-set txt add-record -g $RG -z $ZONE -n @ -v "v=spf1 include:spf.protection.outlook.com -all"
+
+## ------- WEBFLOW ------------
+echo "Setting Webflow DNS"
+# add www CName record for Webflow
+az network dns record-set cname set-record -g $RG -z $ZONE -n www -c "proxy-ssl.webflow.com"
+# add A records for Webflow
+az network dns record-set a add-record -g $RG -z $ZONE -n @ -a "75.2.70.75"
+az network dns record-set a add-record -g $RG -z $ZONE -n @ -a "99.83.190.102"
+
 
 echo "Done."
