7300cd156d7278fda1257793182f7bf1ce4cb51c
Rian <rian@cherry.ai>
2022-02-11 03:21:48 +0000

    Merged PR 380: Persist Local SQL Server data
    - ensure the persist dir exists
    - persist database files

--- app/deploy/sql-database-scripts/local/sqlserver/start-sql-server.sh
+++ app/deploy/sql-database-scripts/local/sqlserver/start-sql-server.sh
@@ -3,9 +3,18 @@ set -e
 echo "> docker ps"
 docker ps
 
+THIS_DIR=$(pwd)
+DATA_DIR="$THIS_DIR/.persist"
+
+echo "SQL Data will be persisted in $DATA_DIR"
+
 echo "Starting SQL Server Docker Container..."
 
 
-sudo docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=YourStrong@Passw0rd" \
+docker start sql1 || \
+   sudo docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=YourStrong@Passw0rd" \
    -p 1433:1433 --name sql1 -h sql1 \
+   -v $DATA_DIR/data:/var/opt/mssql/data \
+   -v $DATA_DIR/log:/var/opt/mssql/log \
+   -v $DATA_DIR/secrets:/var/opt/mssql/secrets \
    -d mcr.microsoft.com/mssql/server:2019-latest
