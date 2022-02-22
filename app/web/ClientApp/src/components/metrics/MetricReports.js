c4fceb62cbbb33d9feebe44e2fea9055344f59b4
Quim Sermonia <quim@cherry.ai>
2022-02-22 00:39:41 +0000

    Merged PR 399: Revamp Metric Reports UI
    - Add report card
    
    ![Screen Shot 2022-02-22 at 8.09.01 AM.png](https://dev.azure.com/cherry-ai/fc25747b-9870-49d7-b9a7-b98a1e1e439d/_apis/git/repositories/e336e387-c1f5-4246-a6c1-e9d790719921/pullRequests/399/attachments/Screen%20Shot%202022-02-22%20at%208.09.01%20AM.png)

--- app/web/ClientApp/src/components/metrics/MetricReports.js
+++ app/web/ClientApp/src/components/metrics/MetricReports.js
@@ -1,33 +1,50 @@
 import React from "react";
+import dayjs from "dayjs";
+
 import { AggregateMetricChartLoader } from "../molecules/charts/AggregateMetricChartLoader";
 import { BigPopup } from "../molecules/popups/BigPopup";
 
-const MetricReports = ({ metric }) => {
-  const [aggregateMetricsChartOpen, setAggregateMetricsChartOpen] =
-    React.useState(false);
+const ReportCard = ({ name, lastUpdated, chart }) => {
+  const [openReport, setOpenReport] = React.useState(false);
 
   return (
     <React.Fragment>
-      <BigPopup
-        isOpen={aggregateMetricsChartOpen}
-        setIsOpen={setAggregateMetricsChartOpen}
-      >
-        <AggregateMetricChartLoader metric={metric} />
-      </BigPopup>
-      <div
-        className="d-flex justify-content-center align-items-center"
-        style={{ minHeight: "50vh" }}
-      >
-        <div>
-          <p>Mean weekly report for {metric.name}</p>
-          <div className="mt-4 text-center">
-            <button
-              onClick={() => setAggregateMetricsChartOpen(true)}
-              className="btn btn-outline-primary"
-              style={{ minWidth: "128px" }}
-            >
-              Show report
-            </button>
+      <div className="card py-5 px-3">
+        <span
+          className="font-weight-bold text-center"
+          style={{ fontSize: "1.25rem" }}
+        >
+          {name}
+        </span>
+        <span className="text-center mt-1">
+          Last updated: {dayjs(lastUpdated).format("DD/MM/YYYY, hh:mm:ss a")}
+        </span>
+        <button
+          onClick={() => setOpenReport(true)}
+          className="btn btn-primary mx-auto mt-4"
+          style={{ minWidth: "128px", maxWidth: "50%" }}
+        >
+          Show report
+        </button>
+        <BigPopup isOpen={openReport} setIsOpen={setOpenReport}>
+          {chart}
+        </BigPopup>
+      </div>
+    </React.Fragment>
+  );
+};
+
+const MetricReports = ({ metric }) => {
+  return (
+    <React.Fragment>
+      <div className="mt-3">
+        <div className="row no-gutters">
+          <div className="col-4 mt-2">
+            <ReportCard
+              name={`Mean Weekly - ${metric.name}`}
+              subheader={metric.lastUpdated}
+              chart={<AggregateMetricChartLoader metric={metric} />}
+            />
           </div>
         </div>
       </div>
