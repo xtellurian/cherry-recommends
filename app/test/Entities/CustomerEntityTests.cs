1f18d7ab51e437be919c3b13dfe235188b7c105d
Rian <rian@cherry.ai>
2022-03-04 03:23:58 +0000

    Merged PR 438: add common ID tests
    add common ID tests

---
+++ app/test/Entities/CustomerEntityTests.cs
@@ -0,0 +1,41 @@
+using SignalBox.Core;
+using Xunit;
+
+namespace SignalBox.Test.Entities
+{
+    public class CustomerEntityTests
+    {
+        [Theory]
+        [InlineData("xx xx xx")]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("??^%")]
+        [InlineData("symbol!")]
+        [InlineData("symbol@")]
+        [InlineData("symbol#")]
+        [InlineData("symbol$")]
+        [InlineData("symbol%")]
+        [InlineData("symbol^")]
+        [InlineData("symbol&")]
+        [InlineData("symbol*")]
+        [InlineData("symbol(")]
+        [InlineData("symbol)")]
+        [InlineData("symbol+")]
+        public void CreateCustomer_BadCustomerId_Throws(string customerId)
+        {
+            Assert.Throws<CommonIdException>(() => new Customer(customerId));
+        }
+
+        [Theory]
+        [InlineData("1")]
+        [InlineData("20")]
+        [InlineData("symbol")]
+        [InlineData("quitealongcustomerid")]
+        [InlineData("underscore_and-hyphen")]
+        public void CreateCustomer_GoodCustomerId(string customerId)
+        {
+            var customer = new Customer(customerId);
+            Assert.Equal(customerId, customer.CustomerId);
+        }
+    }
+}
