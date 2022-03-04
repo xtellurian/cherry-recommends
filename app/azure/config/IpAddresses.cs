78d99daa224a3e8c4b6fdde0a2836316a30b5e7e
Rian <rian@cherry.ai>
2022-03-04 03:39:25 +0000

    Merged PR 440: update IP addresses for retool
    update IP addresses for retool
    
    Related work items: #4743

--- app/azure/config/IpAddresses.cs
+++ app/azure/config/IpAddresses.cs
@@ -7,7 +7,10 @@ namespace SignalBox.Azure
         public static Dictionary<string, string> SqlServerWhitelist = new Dictionary<string, string>
         {
             { "bourkeStreet", "27.32.26.166" },
-            { "coffeeHouse", "149.167.128.243" }
+            { "coffeeHouse", "149.167.128.243" },
+            { "retool1", "52.177.12.28" },
+            { "retool2", "52.177.118.220" },
+            { "retool3", "52.175.251.223" },
         };
     }
 }
