59b05ea3d2ea6ecb31f23b0f68c74ffef81da831
Rian <rian@cherry.ai>
2022-03-18 01:30:31 +0000

    Merged PR 471: Remove 'quick' buttons from Promo Recommender row
    Recommender list is now clean with no buttons
    
    New:
    
    ![image (2).png](https://dev.azure.com/cherry-ai/fc25747b-9870-49d7-b9a7-b98a1e1e439d/_apis/git/repositories/e336e387-c1f5-4246-a6c1-e9d790719921/pullRequests/471/attachments/image%20%282%29.png)
    
    Previous:
    
    ![image.png](https://dev.azure.com/cherry-ai/fc25747b-9870-49d7-b9a7-b98a1e1e439d/_apis/git/repositories/e336e387-c1f5-4246-a6c1-e9d790719921/pullRequests/471/attachments/image.png)

--- app/web/ClientApp/src/components/recommenders/items-recommenders/ItemsRecommendersSummary.js
+++ app/web/ClientApp/src/components/recommenders/items-recommenders/ItemsRecommendersSummary.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
 import {
   Title,
   ErrorCard,
@@ -9,40 +8,10 @@ import {
 } from "../../molecules";
 import { CreateButtonClassic } from "../../molecules/CreateButton";
 import { usePromotionsRecommenders } from "../../../api-hooks/promotionsRecommendersApi";
-import { ButtonGroup } from "../../molecules/buttons/ButtonGroup";
 import { RecommenderRow } from "../RecommenderRow";
-import { tabs } from "./ItemsRecommenderPrimaryNav";
 
 const ItemsRecommenderRow = ({ recommender }) => {
-  const history = useHistory();
-
-  const handleNavigate = (page) => {
-    const queryParams = new URLSearchParams(history.location.search);
-    queryParams.set("tab", page);
-
-    history.push({
-      ...history.location,
-      pathname: `/recommenders/promotions-recommenders/${page}/${recommender.id}`,
-      search: queryParams.toString(),
-    });
-  };
-
-  return (
-    <RecommenderRow recommender={recommender}>
-      <div>
-        <ButtonGroup>
-          {tabs.map((tab) => (
-            <div
-              className="btn btn-outline-primary"
-              onClick={() => handleNavigate(tab.id)}
-            >
-              {tab.label}
-            </div>
-          ))}
-        </ButtonGroup>
-      </div>
-    </RecommenderRow>
-  );
+  return <RecommenderRow recommender={recommender} />;
 };
 export const ItemsRecommendersSummary = () => {
   const itemsRecommenders = usePromotionsRecommenders();
