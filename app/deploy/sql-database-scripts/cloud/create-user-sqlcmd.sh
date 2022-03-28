84f16531b85102e4a798801c59f5199f9adf5fd6
Rian <rian@cherry.ai>
2022-03-28 05:06:33 +0000

    Merged PR 494: Fix: use the server for the stack
    fix: use the server for the stack

--- app/deploy/sql-database-scripts/cloud/create-user-sqlcmd.sh
+++ app/deploy/sql-database-scripts/cloud/create-user-sqlcmd.sh
@@ -13,6 +13,7 @@ cd $APP_PATH/azure
 
 STACK=$(pulumi stack --show-name)
 echo "Using Pulumi Stack $STACK"
+SERVER=$(pulumi stack output SqlServerName)
 SQLSERVERUSERNAME=$(pulumi stack output SqlServerUsername --show-secrets)
 SQLSERVERPASSWORD=$(pulumi stack output SqlServerPassword --show-secrets)
 ADMINUSERNAME=$(pulumi stack output SqlServerAdminUserName --show-secrets)
@@ -23,7 +24,7 @@ READPASSWORD=$(pulumi stack output SqlServerReadPassword --show-secrets)
 cd ../deploy/sql-database-scripts/cloud
 
 echo "Execute User Creation/ Update for $ADMINUSERNAME and $READUSERNAME"
-sqlcmd -S multisql0ec2e42f.database.windows.net -U $SQLSERVERUSERNAME -P $SQLSERVERPASSWORD -d "master" -v AdminUserName=$ADMINUSERNAME AdminPassword=$ADMINPASSWORD ReadUserName=$READUSERNAME ReadPassword=$READPASSWORD -i create-service-principal.sql
-sqlcmd -S multisql0ec2e42f.database.windows.net -U $SQLSERVERUSERNAME -P $SQLSERVERPASSWORD -d "master" -v AdminUserName=$ADMINUSERNAME AdminPassword=$ADMINPASSWORD ReadUserName=$READUSERNAME ReadPassword=$READPASSWORD -i create-db-principal.sql
-sqlcmd -S multisql0ec2e42f.database.windows.net -U $SQLSERVERUSERNAME -P $SQLSERVERPASSWORD -d "tenants" -v AdminUserName=$ADMINUSERNAME AdminPassword=$ADMINPASSWORD ReadUserName=$READUSERNAME ReadPassword=$READPASSWORD -i create-db-principal.sql
-sqlcmd -S multisql0ec2e42f.database.windows.net -U $SQLSERVERUSERNAME -P $SQLSERVERPASSWORD -d "single" -v AdminUserName=$ADMINUSERNAME AdminPassword=$ADMINPASSWORD ReadUserName=$READUSERNAME ReadPassword=$READPASSWORD -i create-db-principal.sql
+sqlcmd -S $SERVER.database.windows.net -U $SQLSERVERUSERNAME -P $SQLSERVERPASSWORD -d "master" -v AdminUserName=$ADMINUSERNAME AdminPassword=$ADMINPASSWORD ReadUserName=$READUSERNAME ReadPassword=$READPASSWORD -i create-service-principal.sql
+sqlcmd -S $SERVER.database.windows.net -U $SQLSERVERUSERNAME -P $SQLSERVERPASSWORD -d "master" -v AdminUserName=$ADMINUSERNAME AdminPassword=$ADMINPASSWORD ReadUserName=$READUSERNAME ReadPassword=$READPASSWORD -i create-db-principal.sql
+sqlcmd -S $SERVER.database.windows.net -U $SQLSERVERUSERNAME -P $SQLSERVERPASSWORD -d "tenants" -v AdminUserName=$ADMINUSERNAME AdminPassword=$ADMINPASSWORD ReadUserName=$READUSERNAME ReadPassword=$READPASSWORD -i create-db-principal.sql
+sqlcmd -S $SERVER.database.windows.net -U $SQLSERVERUSERNAME -P $SQLSERVERPASSWORD -d "single" -v AdminUserName=$ADMINUSERNAME AdminPassword=$ADMINPASSWORD ReadUserName=$READUSERNAME ReadPassword=$READPASSWORD -i create-db-principal.sql
