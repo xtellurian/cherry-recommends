4bc996f303401a65780d69d91352db6a180d5e61
Rian <rian@cherry.ai>
2022-03-29 01:28:44 +0000

    Merged PR 496: fix: don't do DB principal in master database
    fix: don't do DB principal in master database

--- app/deploy/sql-database-scripts/cloud/create-user-sqlcmd.sh
+++ app/deploy/sql-database-scripts/cloud/create-user-sqlcmd.sh
@@ -13,6 +13,7 @@ cd $APP_PATH/azure
 
 STACK=$(pulumi stack --show-name)
 echo "Using Pulumi Stack $STACK"
+
 SERVER=$(pulumi stack output SqlServerName)
 SQLSERVERUSERNAME=$(pulumi stack output SqlServerUsername --show-secrets)
 SQLSERVERPASSWORD=$(pulumi stack output SqlServerPassword --show-secrets)
@@ -25,6 +26,9 @@ cd ../deploy/sql-database-scripts/cloud
 
 echo "Execute User Creation/ Update for $ADMINUSERNAME and $READUSERNAME"
 sqlcmd -S $SERVER.database.windows.net -U $SQLSERVERUSERNAME -P $SQLSERVERPASSWORD -d "master" -v AdminUserName=$ADMINUSERNAME AdminPassword=$ADMINPASSWORD ReadUserName=$READUSERNAME ReadPassword=$READPASSWORD -i create-service-principal.sql
-sqlcmd -S $SERVER.database.windows.net -U $SQLSERVERUSERNAME -P $SQLSERVERPASSWORD -d "master" -v AdminUserName=$ADMINUSERNAME AdminPassword=$ADMINPASSWORD ReadUserName=$READUSERNAME ReadPassword=$READPASSWORD -i create-db-principal.sql
+echo "Created Service Principal."
+# SQL Server does not allow DB Principal to be rcreated on master.
+echo "Creating DB Principal on tenants database"
 sqlcmd -S $SERVER.database.windows.net -U $SQLSERVERUSERNAME -P $SQLSERVERPASSWORD -d "tenants" -v AdminUserName=$ADMINUSERNAME AdminPassword=$ADMINPASSWORD ReadUserName=$READUSERNAME ReadPassword=$READPASSWORD -i create-db-principal.sql
+echo "Creating DB Principal on single database"
 sqlcmd -S $SERVER.database.windows.net -U $SQLSERVERUSERNAME -P $SQLSERVERPASSWORD -d "single" -v AdminUserName=$ADMINUSERNAME AdminPassword=$ADMINPASSWORD ReadUserName=$READUSERNAME ReadPassword=$READPASSWORD -i create-db-principal.sql
