154c697a7478b88b70c62c76c71e2b9ce111cfa9
Quim Sermonia <quim@cherry.ai>
2022-03-29 05:59:11 +0000

    Merged PR 498: Revamp InvokationRow
    - Update InvokationRow UI
    
    ![Screen Shot 2022-03-29 at 12.45.16 PM.png](https://dev.azure.com/cherry-ai/fc25747b-9870-49d7-b9a7-b98a1e1e439d/_apis/git/repositories/e336e387-c1f5-4246-a6c1-e9d790719921/pullRequests/498/attachments/Screen%20Shot%202022-03-29%20at%2012.45.16%20PM.png)
    
    ![Screen Shot 2022-03-29 at 12.45.28 PM.png](https://dev.azure.com/cherry-ai/fc25747b-9870-49d7-b9a7-b98a1e1e439d/_apis/git/repositories/e336e387-c1f5-4246-a6c1-e9d790719921/pullRequests/498/attachments/Screen%20Shot%202022-03-29%20at%2012.45.28%20PM.png)
    
    Related work items: #5214

--- app/web/ClientApp/src/components/recommenders/utils/InvokationLogsUtil.js
+++ app/web/ClientApp/src/components/recommenders/utils/InvokationLogsUtil.js
@@ -1,26 +1,47 @@
-import React from "react";
-import { toShortDate } from "../../../utility/utility";
+import React, { useState } from "react";
 import {
-  EmptyList,
-  ErrorCard,
-  ExpandableCard,
-  Paginator,
-} from "../../molecules";
+  CaretRightFill,
+  CheckCircleFill,
+  XCircleFill,
+} from "react-bootstrap-icons";
+
+import { EmptyList, ErrorCard, Paginator } from "../../molecules";
+import { DateTimeField } from "../../molecules/DateTimeField";
 import { JsonView } from "../../molecules/JsonView";
+import EntityFlexRow from "../../molecules/layout/EntityFlexRow";
+import { BigPopup } from "../../molecules/popups/BigPopup";
 
 const InvokationRow = ({ invokation }) => {
-  const d = toShortDate(invokation.created);
-  const label =
-    (invokation.status || invokation.message || "Invoked") + ` ${d}`;
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  const label = invokation.status || invokation.message || "Invoked";
+
   return (
-    <ExpandableCard
-      label={label}
-      headerClassName={invokation.success ? "bg-success" : "bg-danger"}
-    >
-      <JsonView data={invokation} />
-    </ExpandableCard>
+    <>
+      <EntityFlexRow
+        className="clickable-row cursor-pointer"
+        onClick={() => setIsPopupOpen(true)}
+      >
+        <div className="d-flex align-items-center">
+          <CaretRightFill style={{ color: "var(--cherry-pink)" }} />
+          <span className="ml-2">{label}</span>
+        </div>
+        <div className="d-flex align-items-center">
+          <DateTimeField date={invokation.created} />
+          {invokation.success ? (
+            <CheckCircleFill className="ml-3" color="#2e7d32" />
+          ) : (
+            <XCircleFill className="ml-3" color="#c62828" />
+          )}
+        </div>
+      </EntityFlexRow>
+      <BigPopup isOpen={isPopupOpen} setIsOpen={setIsPopupOpen}>
+        <JsonView data={invokation} />
+      </BigPopup>
+    </>
   );
 };
+
 export const InvokationLogsUtil = ({ invokations }) => {
   return (
     <React.Fragment>
@@ -32,7 +53,6 @@ export const InvokationLogsUtil = ({ invokations }) => {
       {invokations.items && invokations.items.length === 0 && (
         <EmptyList>No invokations.</EmptyList>
       )}
-
       <Paginator {...invokations.pagination} />
     </React.Fragment>
   );
