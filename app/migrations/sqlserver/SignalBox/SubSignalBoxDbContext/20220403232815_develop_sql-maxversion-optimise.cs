ec42d0ee5e05a214a72d58d09aa40cfbd371eea6
Emil Pastor <emil@four2.ai>
2022-04-04 23:59:35 +0000

    Merged PR 505: Optimise View_MaxHistoricCustomerMetricVersion
    Reduce the DB cost for View_MaxHistoricCustomerMetricVersion by removing the views and use ROW_NUMBER analytic function

---
+++ app/migrations/sqlserver/SignalBox/SubSignalBoxDbContext/20220403232815_develop_sql-maxversion-optimise.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace sqlserver.SignalBox.SubSignalBoxDbContext
+{
+    public partial class develop_sqlmaxversionoptimise : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+        CREATE VIEW [dbo].[View_LatestMetrics]
+                AS
+        (
+            SELECT HistoricCustomerMetricId
+                  ,CustomerId
+                  ,BusinessId
+                  ,MaxVersion
+                  ,MetricId
+                  ,NumericValue
+                  ,StringValue
+              FROM
+                  (
+                  SELECT
+                          Id AS HistoricCustomerMetricId
+                          ,CustomerId
+                          ,BusinessId
+                          ,Version AS MaxVersion
+                          ,MetricId
+                          ,NumericValue
+                          ,StringValue
+                          ,ROW_NUMBER() OVER(PARTITION BY MetricId, CustomerId, BusinessId ORDER BY Version DESC) AS RowNumber
+                      FROM HistoricTrackedUserFeatures
+                  ) DT
+              WHERE RowNumber = 1
+        )");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+        DROP VIEW [dbo].[View_LatestMetrics]
+        )");
+        }
+    }
+}
