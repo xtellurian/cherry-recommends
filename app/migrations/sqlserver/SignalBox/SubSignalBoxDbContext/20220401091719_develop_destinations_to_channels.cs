a4f1061a504a289bf4b3dc4e1cdd87bde6c32b85
Cherry Gonzales <cherry.gonzales@cherry.ai>
2022-04-04 05:11:37 +0000

    Merged PR 507: Destinations to channels migration
    - SQL migration to copy Destinations to Channels table
    - SQL migration to copy Destinations-Recommender relationship to RecommenderChannel table

---
+++ app/migrations/sqlserver/SignalBox/SubSignalBoxDbContext/20220401091719_develop_destinations_to_channels.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace sqlserver.SignalBox.SubSignalBoxDbContext
+{
+    public partial class develop_destinations_to_channels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // migrate RecommenderDestinations to Channels
+            migrationBuilder.Sql(
+                @"
+                INSERT INTO [signalbox].[dbo].Channels (Name, LinkedIntegratedSystemId, ChannelType, Discriminator, Endpoint, Created, LastUpdated, EnvironmentId)
+                SELECT Discriminator
+                    ,[ConnectedSystemId]
+                    ,'Webhook'
+                    ,'WebhookChannel'
+                    ,[WebhookDestination_Endpoint]
+                    ,[Created]
+                    ,[LastUpdated]
+                    ,[EnvironmentId]
+                FROM [signalbox].[dbo].[RecommendationDestinations]
+                where Discriminator='WebhookDestination'"
+            );
+
+            // add RecommenderChannel relationship
+            migrationBuilder.Sql(
+                @"
+                INSERT INTO [signalbox].[dbo].[RecommenderChannel] (RecommendersId, ChannelsId)
+                SELECT RecommenderId, c.Id
+                FROM [signalbox].[dbo].[RecommendationDestinations] p
+                JOIN [signalbox].[dbo].[Channels] c ON c.Endpoint = p.WebhookDestination_Endpoint"
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"DELETE from [signalbox].[dbo].Channels
+                Where Name='WebhookDestination'"
+            );
+
+            migrationBuilder.Sql(
+                @"
+                DELETE FROM [signalbox].[dbo].[RecommenderChannel]
+                SELECT RecommenderId, c.Id
+                FROM [signalbox].[dbo].[RecommendationDestinations] p
+                JOIN [signalbox].[dbo].[Channels] c ON c.Endpoint = p.WebhookDestination_Endpoint"
+            );
+        }
+    }
+}
