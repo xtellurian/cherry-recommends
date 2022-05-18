d70c7dada69f4157bc70655dcde90c518d0d2ddc
Cherry Gonzales <cherry.gonzales@cherry.ai>
2022-05-18 03:28:07 +0000

    Merged PR 615: Insert Event to Db
    - Insert event to database without checking if it already exists
    
    Previous code has 18 calls:
    ![image.png](https://dev.azure.com/cherry-ai/fc25747b-9870-49d7-b9a7-b98a1e1e439d/_apis/git/repositories/e336e387-c1f5-4246-a6c1-e9d790719921/pullRequests/615/attachments/image.png)
    
    With modification 17 calls
    ![image (2).png](https://dev.azure.com/cherry-ai/fc25747b-9870-49d7-b9a7-b98a1e1e439d/_apis/git/repositories/e336e387-c1f5-4246-a6c1-e9d790719921/pullRequests/615/attachments/image%20%282%29.png)
    
    Related work items: #5737

--- app/infrastructure/Stores/EF/Stores/EFCustomerEventStore.cs
+++ app/infrastructure/Stores/EF/Stores/EFCustomerEventStore.cs
@@ -46,33 +46,8 @@ namespace SignalBox.Infrastructure.EntityFramework
                 e.EnvironmentId = environmentProvider.CurrentEnvironmentId;
             }
 
-            if (events.Count() == 1)
-            {
-                // no need to chunk
-                var e = events.First();
-                var toRemove = await QuerySet.FirstOrDefaultAsync(_ => _.EventId == e.EventId);
-                if (toRemove != null)
-                {
-                    Set.Remove(toRemove);
-                }
-
-                await Set.AddAsync(e);
-            }
-            else
-            {
-                // need to chunk this query, because too many ids in a single Contains() breaks the db connection.
-                foreach (var chunks in events.ToChunks(255))
-                {
-                    var eventIds = chunks.Select(_ => _.EventId);
-                    var toRemove = QuerySet.Where(_ => eventIds.Contains(_.EventId));
-                    if (toRemove.Any())
-                    {
-                        Set.RemoveRange(await toRemove.ToListAsync());
-                    }
-                }
-
-                await Set.AddRangeAsync(events);
-            }
+            // no need to check if event already exists, just let the db fail if already existing
+            await Set.AddRangeAsync(events);
 
             return events;
         }
