de5458aa7fc66541c19051f9ab7a55ff0d10240e
Cherry Gonzales <cherry.gonzales@cherry.ai>
2022-06-01 21:31:05 +0000

    Merged PR 667: Migration for deferred delivery sending column
    Fix error during Campaign Invoke due to missing 'Sending' field

--- app/migrations/sqlserver/SignalBox/SubSignalBoxDbContext/20220601132044_develop_deferred-delivery-sending.Designer.cs
+++ app/migrations/sqlserver/SignalBox/SubSignalBoxDbContext/20220601132044_develop_deferred-delivery-sending.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SignalBox.Infrastructure;
 
-namespace sqlserver.SignalBox
+namespace sqlserver.SignalBox.SubSignalBoxDbContext
 {
     [DbContext(typeof(SignalBoxDbContext))]
-    partial class SignalBoxDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220601132044_develop_deferred-delivery-sending")]
+    partial class develop_deferreddeliverysending
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ app/migrations/sqlserver/SignalBox/SubSignalBoxDbContext/20220601132044_develop_deferred-delivery-sending.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace sqlserver.SignalBox.SubSignalBoxDbContext
+{
+    public partial class develop_deferreddeliverysending : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastAttemptedDelivery",
+                table: "DeferredDeliveries");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Sending",
+                table: "DeferredDeliveries",
+                type: "bit",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sending",
+                table: "DeferredDeliveries");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastAttemptedDelivery",
+                table: "DeferredDeliveries",
+                type: "datetimeoffset",
+                nullable: true);
+        }
+    }
+}
